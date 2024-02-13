bd6761c0b529fc4976f6d85cbe942cd694005ed5
anuragchhetri <karanchheti@gmail.com>
2024-02-13 21:15:20 +0530

    added redux in chatlist

--- app/components/ChatsList/ChatsList.tsx
+++ app/components/ChatsList/ChatsList.tsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import styles from "./ChatsList.module.scss";
 import Image from 'next/image';
+import { useAppDispatch, useAppSelector } from '@/app/store/hooks';
+import { setUserData } from '@/app/store/slice/userSlice';
+import { useRouter } from 'next/navigation';
 
 
 const ChatsList = () => {
+  const dispatch = useAppDispatch();
+  const router = useRouter()
+  const user = useAppSelector((state) => state.user)
+
+
+
   const val = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+  function handleLogout(){
+    dispatch(setUserData({ email: null, name: null, id: null, isLoggedIn: false }));
+    localStorage.removeItem('tokenchatloom')
+    router.replace('/login')
+  }
   return (
     <div className={styles.chatListContainer}>
       <div className={styles.profileContainer}>
         <div className={styles.profileDetails} >
           <Image width="30" height="30" src={'/profile12.jpg'} alt="profile--v1" />
-          <p>Rohan Singh</p>
+          <p onClick={()=>{handleLogout()}}>{user.name}</p>
         </div>
         <Image width="30" height="30" src="/setting.png" alt="settings--v1" />
       </div>
